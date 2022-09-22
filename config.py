081317ce511871ba8af731ad8b1262415b5aba08
lmilewicz <lukasz.milewicz@gmail.com>
2022-09-22 18:53:35 +0200

    cleanups

--- config.py
+++ config.py
@@ -1,33 +1,3 @@
-'''
-Issues comments:
-- Segmentation fault: ulimit -s to see current value; increase it with ulimit -s <new_value>
-
-'''
-
-
-''' To-do list:
-1)
-- Add last bit on genome that signifies the output layer type   ### done
-- Set up evaluation for:                                        ### done
-    - probabilistic layer                                       ### done
-    - XGboost                                                   ### done
-- Add XGBoost as output layer                                   ### done
-- Improve mutation/sampling with different output layers        ### done --> in MutationAll
-
-Testing:
-- save genomes/models/testing state after specified number of generations/time spend  ### done
-- stop testing and save testing state with an input key         ### NOT done
-- enable testing from saved point                               ### done
-
-
-2)
-- Add probabilistic Dense and Conv layers
-- Different Conv layers/conv filers
-- How to use graph to show different layer types. Current graph is focused on connection 
-
-3) Different modules connections? Non sequential modules connection?
-
-'''
 
 import numpy as np
 import tensorflow_probability as tfp
@@ -42,7 +12,7 @@ import test
 class Config():
     def __init__(self, argv=[]):
         ### Model settings      ###
-        self.dataset = 'cifar10' # 'mnist' 'cifar10'
+        self.dataset = 'cifar10' # 'cifar10'  'mnist' 'cifar10_corrupted' 'cifar100'
         self.enable_xgboost = False
         self.batch_size = 32
 
